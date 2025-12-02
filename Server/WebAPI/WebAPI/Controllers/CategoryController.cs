b682b1f600a2159fbe6958e13ebee485f7991dbb
Muhammed Najmal <mhdnajmal197@gmail.com>
2025-12-03 00:08:22 +0530

    add product controller

---
+++ Server/WebAPI/WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.DTOs.Categories;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryDto dto)
+        {
+            var category = new Categories
+            {
+                Name = dto.Name,
+                Description = dto.Description
+            };
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Category created successfully");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CategoryDto dto)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            category.Name = dto.Name;
+            category.Description = dto.Description;
+            _context.SaveChanges();
+            return Ok("Category updated successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Ok("Category deleted successfully");
+        }
+    }
+}
--- Server/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ Server/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e45ed5cc0fd5b272d278ef65d8e18490b9ee46a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dce8dbdf8220d21327e6c49ac85e8952f948a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
