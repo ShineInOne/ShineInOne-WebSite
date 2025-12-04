1304fc9d74e81a9623690864e28a86eb7bebd5d5
Muhammed Najmal <mhdnajmal197@gmail.com>
2025-12-04 14:06:48 +0530

    Product list done

---
+++ Client/Web-Dashboard/src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private api = environment.apiUrl;
+  
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.api}/Product`);
+  }
+}
