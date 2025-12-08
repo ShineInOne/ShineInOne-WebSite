d8eb13ed193c416fd299a8d9447371b6be47c738
Muhammed Najmal <mhdnajmal197@gmail.com>
2025-12-08 13:25:21 +0530

    Hero Section updated

--- Client/Web-Dashboard/src/app/app.ts
+++ Client/Web-Dashboard/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductsComponent],
+  imports: [HeroComponent, ProductsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
