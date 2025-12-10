918bb6b87b88f8a6f59b8e0c9243c7ee3058fe14
Adish <adishbeypore@gmail.com>
2025-12-10 11:43:38 +0530

    created header footer and working links + about us

--- Client/Web-Dashboard/src/app/components/header/header.component.ts
+++ Client/Web-Dashboard/src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  showPopup = false;
+  popupType: string | null = null;
 
+  openPopup(type: string) {
+    this.popupType = type;
+    this.showPopup = true;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+    this.popupType = null;
+  }
 }
