02baa9c7f12ea1168c95fffd41d42292575f61a8
Adish <adishbeypore@gmail.com>
2025-12-25 13:37:51 +0530

    cart-ui

--- Client/Web-Dashboard/src/app/app.ts
+++ Client/Web-Dashboard/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
 import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from "./components/header/header.component";
@@ -8,7 +8,8 @@ import { FeaturesComponent } from "./components/features/features.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent, ProductsComponent, HeaderComponent, FooterComponent, FeaturesComponent],
+  standalone: true,
+  imports: [HeroComponent, ProductsComponent, HeaderComponent, FooterComponent, FeaturesComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
